1c0befd7dcd307f271e14208f6816a61d1a6df87
will-ing <will-ing@github.com>
2020-08-27 19:59:16 -0700

    added a little css for practice

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views import HomePageView, PostDetailPageView
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('base/', BasePageView.as_view(), name='base'),
     path('', HomePageView.as_view(), name='home'),
     path('detail/<int:pk>/', PostDetailPageView.as_view(), name='detail'),
-]
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,6 @@ from django.views.generic import TemplateView, ListView, DetailView
 from .models import Blog
 
 
-# class BasePageView(TemplateView):
-#     template_name = 'base.html'
-#     model = Blog
-
-
 class HomePageView(ListView):
     template_name = 'home.html'
     model = Blog
@@ -16,8 +11,3 @@ class HomePageView(ListView):
 class PostDetailPageView(DetailView):
     model = Blog
     template_name = 'post_detail.html'
-
-
-# class ModelDetailView(DetailView):
-#     model = Blog
-#     template_name = "post_detail.html"
