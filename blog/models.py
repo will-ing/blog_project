e582a9a9fa87ff644e2c2b458f9c0c7a13e8af42
will-ing <will-ing@github.com>
2020-08-29 14:29:22 -0700

    added test

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=64)
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
     body = models.TextField(max_length=264)
 
     def __str__(self):
--- blog/tests.py
+++ blog/tests.py
@@ -1,8 +1,10 @@
-from django.test import SimpleTestCase
+from .models import Blog
+from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
-class BlogTest(SimpleTestCase):
+class BlogTest(TestCase):
 
     def test_home_page_status(self):
         self.help_status_code('home')
@@ -20,3 +22,35 @@ class BlogTest(SimpleTestCase):
         response = self.client.get(url)
         self.assertTemplateUsed(response, temp_name)
         self.assertTemplateUsed(response, 'base.html')
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test',
+            password='pass'
+        )
+
+        self.blog = Blog.objects.create(
+            title='test title',
+            author=self.user,
+            body='test desc',
+        )
+
+    def test_content(self):
+        self.assertEqual(self.blog.title, 'test title')
+        self.assertEqual(self.blog.author, self.user)
+        self.assertEqual(self.blog.body, 'test desc')
+
+    def test_create(self):
+        response = self.client.post(reverse('create'), {
+            'title': 'test title',
+            'author': self.user,
+            'body': 'test desc',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test title')
+        self.assertContains(response, 'test desc')
+        self.assertTemplateUsed(response, 'create.html')
+
+    def test_trip_delete_view(self):
+        response = self.client.get(reverse('delete', args='1'))
+        self.assertEqual(response.status_code, 200)
